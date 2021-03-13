887af4f48c41ba492c0fbbc81ad332ea2e73df3f
Yoshiki Yamashita <yoshikiyamashita33@gmail.com>
2021-03-13 14:57:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <h1>Redux-Saga!</h1>
       <p>Redux-Saga manages async requests such as data fetching</p>
       { user === undefined && <h1>Now Loading...</h1>}
-      { user === false && <h1>Error, fetch failed</h1> }
+      { user === false && <h1>Error, fetch failed</h1>}
       { user && <h1>Welcom, {user.firstname}</h1>}
     </div>
   );
